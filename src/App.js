b5bcbeb459af78b5fb3142ee90d9511027fb7e00
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2021-03-14 16:56:51 +0100

    Component function:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
+import Header from "./header/Header";
 
 function App() {
   return (
     <div className="App">
-      <h1>React Ecommerce</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquid amet beatae consequatur cum deserunt doloribus dolorum ea excepturi facere ipsum, itaque molestias, officiis provident quaerat, reprehenderit similique tempora temporibus!</p>
+      <Header/>
     </div>
   );
 }
---
+++ src/header/Header.js
@@ -0,0 +1,19 @@
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header wrapper">
+      <span className="material-icons">
+        menu
+      </span>
+      <div>
+        React Ecommerce
+      </div>
+      <span className="material-icons">
+        shopping_cart
+      </span>
+    </header>
+  );
+}
+
+export default Header;
