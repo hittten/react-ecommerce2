ea6d4db31e94230ca84fbfcb6a967d168aaf9c4d
Gilberto López Ambrosino <gilberto.amb@gmail.com>
2021-03-20 13:53:46 +0100

    Hoots: header & product list

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,51 +1,39 @@
 import './Header.css';
-import * as React from "react";
+import {useState} from 'react';
 import {Link} from "react-router-dom";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
+function Header() {
+  const [navOpen, setNavOpen] = useState(false);
 
-    this.state = {
-      navOpen: false,
-    }
-  }
-
-  render() {
-    return (
-      <header className={`wrapper ${this.state.navOpen ? 'nav-open' : ''}`}>
-        <span className="material-icons" onClick={() => this.state.navOpen ? this.openNav(false) : this.openNav(true)}>
-          {this.state.navOpen ? 'close' : 'menu'}
+  return (
+    <header className={`wrapper ${navOpen ? 'nav-open' : ''}`}>
+        <span className="material-icons" onClick={() => navOpen ? setNavOpen(false) : setNavOpen(true)}>
+          {navOpen ? 'close' : 'menu'}
         </span>
-        <nav>
-          <ul>
-            <li onClick={() => this.openNav(false)}>
-              <Link to="/">
-                <span className="material-icons">view_list</span> Products
-              </Link>
-            </li>
-            <li onClick={() => this.openNav(false)}>
-              <Link to="/shopping-cart">
-                <span className="material-icons">shopping_cart</span> Car
-              </Link>
-            </li>
-          </ul>
-        </nav>
-        <div>
-          React Ecommerce
-        </div>
-        <Link to="/shopping-cart">
-          <span className="material-icons" onClick={() => this.openNav(false)}>
+      <nav>
+        <ul>
+          <li onClick={() => setNavOpen(false)}>
+            <Link to="/">
+              <span className="material-icons">view_list</span> Products
+            </Link>
+          </li>
+          <li onClick={() => setNavOpen(false)}>
+            <Link to="/shopping-cart">
+              <span className="material-icons">shopping_cart</span> Car
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        React Ecommerce
+      </div>
+      <Link to="/shopping-cart">
+          <span className="material-icons" onClick={() => setNavOpen(false)}>
             shopping_cart
           </span>
-        </Link>
-      </header>
-    );
-  }
-
-  openNav(value) {
-    this.setState({navOpen: value});
-  }
+      </Link>
+    </header>
+  )
 }
 
 export default Header;
--- src/product-list/ProductList.js
+++ src/product-list/ProductList.js
@@ -1,99 +1,81 @@
 import './ProductList.css';
-import * as React from "react";
+import {useState, useEffect} from "react"
 import {uppercase} from "../utils/uppercase";
 import {euroCurrency} from "../utils/euro-currency";
 import {ReactComponent as Loading} from '../assets/spinner.svg';
 
-class ProductList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      gridView: false,
-      error: null,
-      isLoaded: false,
-      products: [],
-    }
-  }
+function ProductList(props) {
+  const [gridView, setGridView] = useState(false)
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [products, setProducts] = useState([])
 
-  componentDidMount() {
-    this.props.products()
+  useEffect(() => {
+    props.products()
       .then(
         (result) => {
-          this.setState({
-            isLoaded: true,
-            products: result
-          });
+          setIsLoaded(true)
+          setProducts(result)
         },
-        // Nota: es importante manejar errores aquí y no en
-        // un bloque catch() para que no interceptemos errores
-        // de errores reales en los componentes.
         (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+          setIsLoaded(false)
+          setError(error)
         }
       )
-  }
+  }, [props, props.products])
 
-  render() {
-    return (
-      <div className="product-list">
-        <div className="views">
-          <span className={`material-icons ${!this.state.gridView ? 'selected' : ''}`}
-                onClick={() => this.setState({gridView: false})}>
-            view_list
-          </span>
-          <span className={`material-icons ${this.state.gridView ? 'selected' : ''}`}
-                onClick={() => this.setState({gridView: true})}>
-            view_module
-          </span>
-        </div>
-        <ul className={this.state.gridView ? 'grid' : ''}>
-          {this.state.error ? <div>Error: {this.state.error.message}</div> :
-            !this.state.isLoaded ? <Loading/> :
-              this.state.products.map((product, index) =>
-                <li key={index}>
-                  <h2>{uppercase(product.name)}</h2>
-                  <img src={product.image} alt={product.name}/>
-                  <p>
-                    ({new Date(product.createdAt).toLocaleDateString()})
-                    - {product.description.length > 150 ? product.description.slice(0, 150) + '...' : product.description}
-                  </p>
-                  <div className="price">{euroCurrency(product.price)}</div>
-                  <div>
-                    <button className="button"
-                            onClick={(e) => this.handleButtonClick(e, product, index)}>{this.props.buttonText}</button>
-                  </div>
-                </li>)}
-        </ul>
-      </div>
-    );
-  }
-
-  handleButtonClick(e, product, index) {
+  function handleButtonClick(e, product, index) {
     e.target.disabled = true
-    if (!this.props.update) {
-      return this.props.handleButtonClick(product)
+    if (!props.update) {
+      return props.handleButtonClick(product)
         .finally(() => {
           e.target.disabled = false
         })
     }
 
-    this.props.handleButtonClick(product)
+    props.handleButtonClick(product)
       .then(() => {
-        this.setState((state) => {
-          const newProducts = [...state.products];
-          newProducts.splice(index, 1)
-          return {
-            products: newProducts,
-          }
-        })
+        const newProducts = [...products];
+        newProducts.splice(index, 1)
+        setProducts(newProducts)
       })
       .finally(() => {
         e.target.disabled = false
       })
   }
+
+  return (
+    <div className="product-list">
+      <div className="views">
+          <span className={`material-icons ${!gridView ? 'selected' : ''}`}
+                onClick={() => setGridView(false)}>
+            view_list
+          </span>
+        <span className={`material-icons ${gridView ? 'selected' : ''}`}
+              onClick={() => setGridView(true)}>
+            view_module
+          </span>
+      </div>
+      <ul className={gridView ? 'grid' : ''}>
+        {error ? <div>Error: {error.message}</div> :
+          !isLoaded ? <Loading/> :
+            products.map((product, index) =>
+              <li key={index}>
+                <h2>{uppercase(product.name)}</h2>
+                <img src={product.image} alt={product.name}/>
+                <p>
+                  ({new Date(product.createdAt).toLocaleDateString()})
+                  - {product.description.length > 150 ? product.description.slice(0, 150) + '...' : product.description}
+                </p>
+                <div className="price">{euroCurrency(product.price)}</div>
+                <div>
+                  <button className="button"
+                          onClick={(e) => handleButtonClick(e, product, index)}>{props.buttonText}</button>
+                </div>
+              </li>)}
+      </ul>
+    </div>
+  );
 }
 
 export default ProductList;
